60c4654d543dd6bc040f1615f18cd86813fe69c0
SaveraHasan <saverahasan5@gmail.com>
2023-09-08 03:05:00 +0500

    tic tac toe game

---
+++ lib/constant/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 255, 0, 166);
+  static Color SecondaryColor = Color.fromARGB(255, 106, 233, 255);
+  static Color accentColor = Color.fromARGB(255, 254, 233, 0);
+  static Color accentColor2 = Color.fromARGB(255, 50, 224, 255);
+
+  
+}
